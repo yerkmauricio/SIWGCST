aa4a2cfc28726018894bf14ac37b929422286c89
rudy mauricio <yerkm@yerk.yerk>
2023-11-01 09:42:57 -0400

    Mensaje  de  cambios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,12 @@ class DatabaseSeeder extends Seeder
     {
     
         // llamando al metodo seeder de permisos
-        //$this->call(RoleSeeder::class);
+        $this->call(RoleSeeder::class);
 
         $this->call(AreaSeeder::class);
         $this->call(NJerarquicoSeeder::class);
         $this->call(CargoSeeder::class);
-        //$this->call(EmpleadosSeeder::class);
+        $this->call(EmpleadosSeeder::class);
         // $this->call(AlimentosSeeder::class);
         // $this->call(DescuentoSeeder::class);
         // $this->call(DestinosSeeder::class);
@@ -31,6 +31,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(ObsNoincludeSeeder::class);
         // $this->call(TransporteSeeder::class);
         // $this->call(ToursSeeder::class);
-       // $this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -14,18 +15,26 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $usuarios = [
-            [
-                'name' => 'rudy mauricio', 'email' => 'yerkmauricio@gmail.com',
-                'password' => hash::make('contraseña_segura'), // Genera una contraseña segura
-                'empleado_id' => 1,
-            ],
-            [
-                'name' => 'daysi llusco', 'email' => 'daysi@gmail.com',
-                'password' => Hash::make('otra_contraseña_segura'), // Genera otra contraseña segura
-                'empleado_id' => 2,
-            ]
-        ];
-        User::insert($usuarios);
+        $user1 = User::create([
+            'name' => 'rudy mauricio',
+            'email' => 'yerkmauricio@gmail.com',
+            'password' => Hash::make('contraseña_segura_1'),
+            'empleado_id' => 1,
+        ]);
+
+        $user2 = User::create([
+            'name' => 'daysi llusco',
+            'email' => 'daysi@gmail.com',
+            'password' => Hash::make('otra_contraseña_segura'),
+            'empleado_id' => 2,
+        ]);
+
+        // Asignar el rol "administrador" a los usuarios si es necesario
+        $role = Role::where('name', 'administrador')->first();
+
+        if ($role) {
+            $user1->assignRole($role);
+            $user2->assignRole($role);
+        }
     }
 }
